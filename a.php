2a8ca53f6186511a193887a24bfb921e1a1e5696
Shinki_Hiroyuki <Shinki_Hiroyuki@sol.konami>
2017-01-19 17:23:20 +0900

    add

---
+++ a.php
@@ -0,0 +1,10 @@
+<?php
+include "b.php";
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+
+var_dump($undefined);
---
+++ b.php
@@ -0,0 +1,19 @@
+<?php
+$a = "a";
+$b = ["b"];
+$c = new DateTime();
+$d = fD();
+$e = fE();
+
+/**
+ * @return array
+ */
+function fD()
+{
+    return "d";
+}
+
+function fE()
+{
+    return ["e"];
+}
